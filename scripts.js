a976d835ec2bb9f8024fc20e45127902c59427f4
tornedoboy <tornedoboy@gmail.com>
2021-06-06 15:44:39 -0700

    its working but some errors

--- scripts.js
+++ scripts.js
@@ -1,25 +1,60 @@
 function a() {
     const comp = ["rock", "papper", "scissor"];
-    computer();
+    const u = user();
+    const c = computer();
+    game();
+    //computer();
     //user();
 
-    function user() {
-        const userinput = prompt("welcome to the game plz input rock or paper or scissor");
+    function user(userinput) {
+        userinput = prompt("welcome to the game plz input rock or paper or scissor");
         return userinput
 
 
     }
 
-    function computer() {
-        const copinput = comp[Math.floor(Math.random() * comp.length)];
-        console.log(copinput);
+    function computer(copinput) {
+        copinput = comp[Math.floor(Math.random() * comp.length)];
+        return copinput
 
 
 
     }
 
     function game() {
+        if (u == c) {
+            alert("draw" + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+
+        } else if (u == 'rock' && c == "scissor") {
+            alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+            if (u == 'scissor' && c == "rock") {
+                alert('user wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+
+            }
+
+        } else if (u == 'paper' && c == "scissor") {
+            alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+            if (u == 'scissor' && c == "paper") {
+                alert('user wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+
+            }
 
 
+        } else if (u == 'rock' && c == "paper") {
+            alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+            if (u == 'paper' && c == "rock") {
+                alert('user wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+
+            }
+
+
+
+
+        } else if (u == 'scissor' && c == "paper") {
+            alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+            if (u == 'paper' && c == "scissor") {
+                alert('user wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+            }
+        }
     }
 }
