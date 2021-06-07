01b0f779fdc1b884755c9441c792104867352abc
tornedoboy <tornedoboy@gmail.com>
2021-06-07 14:56:31 -0700

    working on btn

--- scripts.js
+++ scripts.js
@@ -1,71 +1,80 @@
 function a() {
-    const comp = ["rock", "papper", "scissor"];
-    const u = user();
-    const c = computer();
-    game();
-    //computer();
-    //user();
+    for (let i = 1; i <= 5; i++) {
+        const comp = ["rock", "papper", "scissor"];
+        const u = user();
+        const c = computer();
+        const buttonr = document.querySelectorAll('.btn');
 
-    function user(userinput) {
-        userinput = prompt("welcome to the game plz input rock or paper or scissor");
-        return userinput
+        //game();
+        //computer();
+        user();
+
+        function user(userinput) {
+
+            userinput = undefined;
+            buttonr.forEach((buttonr) => {
+                buttonr.addEventListener('click', () => {
+                    userinput = buttonr.getAttribute('data-choice')
+                    console.log(userinput);
+                });
+            });
+            //userinput = prompt("welcome to the game plz input rock or paper or scissor");
+            return userinput
 
 
-    }
 
-    function computer(copinput) {
-        copinput = comp[Math.floor(Math.random() * comp.length)];
-        return copinput
+        }
+
+        function computer(copinput) {
+            copinput = comp[Math.floor(Math.random() * comp.length)];
+            return copinput
 
 
 
-    }
+        }
 
-    function game()
+        function game()
 
-    {
-        if (u === c) {
-            alert("draw" + ' ' + 'computer input' + '  ' + c + ' ' + 'user input' + ' ' + u);
+        {
+            if (u === c) {
+                alert("draw" + ' ' + 'computer input' + '  ' + c + ' ' + 'user input' + ' ' + u);
 
-        } else if (u === "scissor") {
+            } else if (u === "scissor") {
 
-            if (c === 'rock') {
-                alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+                if (c === 'rock') {
+                    alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
 
-            } else {
-                alert('user wins' + ' ' + 'computer input' + '  ' + c + ' ' + 'user input' + ' ' + u);
+                } else {
+                    alert('user wins' + ' ' + 'computer input' + '  ' + c + ' ' + 'user input' + ' ' + u);
+
+                }
+
+            } else if (u === "rock") {
+                //alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+                if (c === "paper") {
+                    alert('user wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+
+                } else {
+                    alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+
+                }
+
+
+            } else if (u === "paper") {
+                //alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+                if (c == 'sicssor') {
+                    alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+
+                } else {
+                    alert('user wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
+
+                }
+
+
+
 
             }
 
-        } else if (u === "rock") {
-            //alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
-            if (c === "paper") {
-                alert('user wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
-
-            } else {
-                alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
-
-            }
-
-
-        } else if (u === "paper") {
-            //alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
-            if (c == 'sicssor') {
-                alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
-
-            } else {
-                alert('user wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
-
-            }
-
-
-
-
-        } //else if (u == 'scissor' && c == "paper") {
-        // alert('computer wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
-        // if (u == 'paper' && c == "scissor") {
-        //     alert('user wins' + ' ' + 'computer input' + '  ' + c + 'user input' + ' ' + u);
-        // }
-
+        }
     }
 }
