20213d7d4bb04d9157a03ae3630853918eacc4f8
tornedoboy <84871658+tornedoboy@users.noreply.github.com>
2021-06-09 21:20:52 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,55 @@
 function ab() {
+
     const comp = ["rock", "paper", "scissor"];
+    let moves_left = 5;
+    let playerscr = 0;
+    let computerscr = 0;
+    let pwin = "GAME OVER  Player wins";
+    let cwin = "GAME OVER  Computer wins";
+
+
 
     const buttons = document.querySelectorAll('.btn');
     let userinput = undefined;
     buttons.forEach((opt) => {
         opt.addEventListener('click', () => {
             userinput = opt.getAttribute('data-choice');
-            console.log(userinput)
+            //console.log(userinput)
 
-            //function run() {
-            // for (i = 1; i < 5; i++) {
+
             game();
-            // };
-            //}
+
         });
     });
 
+    function results() {
+        let m = moves_left;
+        if (m == 0) {
+            if (playerscr > computerscr) {
+
+
+                document.getElementById('hello').setAttribute("style", "visibility:hidden");
+                document.getElementById('bye').setAttribute("style", "visibility:hidden");
+                document.getElementById('hi').setAttribute("style", "visibility:hidden");
+
+                document.getElementById('end').innerHTML = pwin;
+
+                //alert('game over   player wins')
+            } else {
+
+                document.getElementById('hello').setAttribute("style", "visibility:hidden");
+                document.getElementById('bye').setAttribute("style", "visibility:hidden");
+                document.getElementById('hi').setAttribute("style", "visibility:hidden");
+                document.getElementById('end').innerHTML = cwin;
+                //alert('game over computer wins')
+            }
+
+
+        } else {
+            alert('keep playing ' + 'Moves left -' + m)
+        }
+
+    }
 
     function computer(copinput) {
         copinput = comp[Math.floor(Math.random() * comp.length)];
@@ -27,24 +61,42 @@ function ab() {
 
 
     {
-
         const c = computer();
+
+
+
         if (userinput === c) {
             alert("draw" + ' ' + 'computer input -' + '  ' + c + ' ' + ' user input -' + ' ' + userinput);
+            results();
 
         } else if ((userinput === "scissor" && c === "paper") ||
             (userinput === "rock" && c === "scissor") ||
             (userinput === "paper" && c === "rock"))
 
         {
+            playerscr++;
+            moves_left--;
+            document.getElementById("ply").innerHTML = playerscr;
+            document.getElementById("mov").innerHTML = moves_left;
+            console.log('ply ' + playerscr)
+            console.log('mov ' + moves_left)
             alert('player wins' + ' ' + 'computer input -' + '  ' + c + ' user input -' + ' ' + userinput);
+            results();
 
 
 
         } else {
+            computerscr++;
+            moves_left--;
+            document.getElementById("com").innerHTML = computerscr;
+            document.getElementById("mov").innerHTML = moves_left;
+            console.log('com ' + computerscr)
+            console.log('mov ' + moves_left)
             alert('computer wins' + ' ' + 'computer input -' + '  ' + c + ' user input -' + ' ' + userinput);
+            results();
 
         }
+
 
 
     }
